3c280c359d141b9c355f63ec8b9922ee5d9c20c9
Ryosuke Takahashi <osaru2002729@gmail.com>
2022-02-13 20:04:07 +0900

    Isolate JS into a separate file

---
+++ page/js/index.js
@@ -0,0 +1,24 @@
+document.querySelector("form").addEventListener("submit", async (event) => {
+  event.preventDefault();
+  await sendJSON("/summon", {
+    url: event.target.url.value,
+    count: parseInt(event.target.count.value),
+  });
+  location.reload();
+});
+
+document.querySelectorAll(".kill").forEach((a) =>
+  a.addEventListener("click", async (event) => {
+    event.preventDefault();
+    await sendJSON("/kill", { id: a.dataset.id });
+    location.reload();
+  })
+);
+
+function sendJSON(url, json) {
+  return fetch(url, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(json),
+  });
+}
