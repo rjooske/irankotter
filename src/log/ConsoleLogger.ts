e1c4c0a9c9993896937f8d960ce7e42026a9010a
Ryosuke Takahashi <osaru2002729@gmail.com>
2022-02-22 23:02:47 +0900

    ConsoleLogger uses string for the ID

--- src/log/ConsoleLogger.ts
+++ src/log/ConsoleLogger.ts
@@ -1,7 +1,7 @@
 import { Logger } from "./Logger";
 
 export class ConsoleLogger implements Logger {
-  constructor(private readonly id: number) {}
+  constructor(private readonly id: string) {}
 
   log(...args: any[]) {
     console.log([new Date().toISOString(), this.id, ...args].join(" "));
