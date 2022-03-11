a2e39f8a20d359b4b923b3a760c25418b09fb2bd
Ryosuke Takahashi <rjooske@gmail.com>
2022-03-11 22:26:18 +0900

    DrednotBot should jump

--- src/domain/drednot/DrednotBot.ts
+++ src/domain/drednot/DrednotBot.ts
@@ -1,5 +1,6 @@
 import { DrednotBotEventListener } from "./DrednotBotEventListener";
 
 export interface DrednotBot {
+  jump(): Promise<void>;
   setEventListener(listener: DrednotBotEventListener): void;
 }
