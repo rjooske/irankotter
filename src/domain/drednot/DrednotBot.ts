8f17c2ba1a9747167ad679e0a9609b432e56ab6a
Ryosuke Takahashi <rjooske@gmail.com>
2022-03-11 23:45:29 +0900

    Add mouse operations in DrednotBot

--- src/domain/drednot/DrednotBot.ts
+++ src/domain/drednot/DrednotBot.ts
@@ -1,6 +1,9 @@
 import { DrednotBotEventListener } from "./DrednotBotEventListener";
 
 export interface DrednotBot {
+  moveMouse(x: number, y: number): Promise<void>;
+  pressMouseButton(): Promise<void>;
+  releaseMouseButton(): Promise<void>;
   jump(): Promise<void>;
   setEventListener(listener: DrednotBotEventListener): void;
 }
