686fc06402ffdaad6730db49821b859583d300e8
Ryosuke Takahashi <rjooske@gmail.com>
2022-03-15 22:22:00 +0900

    Test mouse service

---
+++ src/infrastructure/mouse/MouseService.ts
@@ -0,0 +1,35 @@
+import { MouseEventListener } from "../../domain/mouse/MouseEventListener";
+import * as domain from "../../domain/mouse/MouseService";
+
+export class MouseService implements domain.MouseService {
+  private readonly listeners: MouseEventListener[] = [];
+
+  constructor() {
+    setInterval(() => {
+      const angle = (Math.PI / 2) * (Date.now() / 1000);
+      const x = 0.5 * Math.cos(angle) + 0.5;
+      const y = 0.5 * Math.sin(angle) + 0.5;
+
+      for (const listener of this.listeners) {
+        listener.onMouseMove({
+          x,
+          y,
+          screenWidth: 1,
+          screenHeight: 1,
+        });
+      }
+    }, 50);
+  }
+
+  addEventListener(listener: MouseEventListener): void {
+    this.listeners.push(listener);
+  }
+
+  removeEventListener(listener: MouseEventListener): void {
+    for (let i = 0; i < this.listeners.length; i++) {
+      if (this.listeners[i] === listener) {
+        this.listeners.splice(i, 1);
+      }
+    }
+  }
+}
