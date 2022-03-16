7d6b0b222383475f1a623addbdf4114a43a08e15
Ryosuke Takahashi <rjooske@gmail.com>
2022-03-16 21:57:39 +0900

    Add left and right mouse button distinction

--- src/domain/drednot/DrednotBot.ts
+++ src/domain/drednot/DrednotBot.ts
@@ -1,9 +1,10 @@
+import { MouseButton } from "../mouse/MouseButton";
 import { DrednotBotEventListener } from "./DrednotBotEventListener";
 
 export interface DrednotBot {
   moveMouse(x: number, y: number): Promise<void>;
-  pressMouseButton(): Promise<void>;
-  releaseMouseButton(): Promise<void>;
+  pressMouseButton(button: MouseButton): Promise<void>;
+  releaseMouseButton(button: MouseButton): Promise<void>;
   jump(): Promise<void>;
   setScreenSize(width: number, height: number): Promise<void>;
   setEventListener(listener: DrednotBotEventListener): void;
--- src/domain/mouse/MouseEventListener.ts
+++ src/domain/mouse/MouseEventListener.ts
@@ -1,7 +1,8 @@
+import { MouseButton } from "./MouseButton";
 import { MouseMoveEvent } from "./MouseMoveEvent";
 
 export interface MouseEventListener {
   onMouseMove(event: MouseMoveEvent): void;
-  onMouseButtonDown(): void;
-  onMouseButtonUp(): void;
+  onMouseButtonDown(button: MouseButton): void;
+  onMouseButtonUp(button: MouseButton): void;
 }
