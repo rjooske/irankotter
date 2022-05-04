b7d6e0c7d81e9db6c346934f93bef4feb6097023
Ryosuke Takahashi <rjooske@gmail.com>
2022-05-04 18:28:11 +0900

    Use an SSL certificate

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 // import commandLineArgs from "command-line-args";
 
-import { createServer } from "http";
+import { readFileSync } from "fs";
+import { createServer } from "https";
 import { HealerApplication } from "./application/healer/HealerApplication";
+import { TurretOperatorApplication } from "./application/turret/TurretOperatorApplication";
 import { HealerGroup } from "./domain/healer/HealerGroup";
+import { TurretOperatorGroup } from "./domain/turret/TurretOperatorGroup";
 import { DrednotBotFactory } from "./infrastructure/drednot/DrednotBotFactory";
+import { MouseService } from "./infrastructure/mouse/MouseService";
 import { HealerController } from "./presentation/controller/healer/HealerController";
 
 // interface Options {
@@ -182,14 +186,40 @@ import { HealerController } from "./presentation/controller/healer/HealerControl
   //   "right"
   // );
 
-  const server = createServer();
+  const server = createHttpsServer(
+    "cert/localhost/localhost.crt",
+    "cert/localhost/localhost.key"
+  );
+
+  const logger = (s: string) => {
+    const time = new Date(Date.now()).toISOString();
+    console.log(`${time} | ${s}`);
+  };
+
   const drednotBotFactory = new DrednotBotFactory(false);
-  const logger = (s: string) => console.log(s);
-  const healerController = new HealerController(
+  const mouseService = new MouseService(server, logger);
+
+  new HealerController(
     server,
     new HealerApplication(new HealerGroup(drednotBotFactory, logger)),
     logger
   );
+  new TurretOperatorApplication(
+    new TurretOperatorGroup(drednotBotFactory, mouseService, logger)
+  );
 
-  server.listen(8080, () => logger("Server started"));
+  const port = 8080;
+  server.listen(port, () => logger(`server started at ${port}`));
 })();
+
+function createHttpsServer(certificatePath: string, keyPath: string) {
+  return createServer(
+    { cert: readFileSync(certificatePath), key: readFileSync(keyPath) },
+    (request, response) => {
+      if (request.url === "/") {
+        response.writeHead(200);
+        response.end("OK");
+      }
+    }
+  );
+}
